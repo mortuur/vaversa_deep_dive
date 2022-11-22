1cfdfcc0097436d2902c8b10471e5ef69303a8dc
Henry Benaye <57096397+HenryBenaye@users.noreply.github.com>
2022-11-22 10:17:43 +0100

    model activity

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Plant;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,7 +15,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+         Plant::factory(10)->create();
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
