6537b457dc959b2faebb5a332d7821a5270b385a
bart_van_der_plas <bart.vanderplas@student.rocva.nl>
2022-11-22 12:23:15 +0100

    notification op window

---
+++ notification.js
@@ -0,0 +1,11 @@
+const button = document.getElementById("button")
+
+button.addEventListener("click", () =>{
+    Notification.requestPermission().then(perm => {
+        if (perm === "granted") {
+            new Notification("test test"),{                
+            }
+        }
+    })
+})
+// notification to mobile?
