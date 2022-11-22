519557a6705df385c8eef6884ad6cbe88d91e5d6
Henry Benaye <57096397+HenryBenaye@users.noreply.github.com>
2022-11-22 14:37:05 +0100

    cronjob

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,6 +2,8 @@
 
 namespace App\Console;
 
+use App\Jobs\ActivityNotification;
+use App\Models\Activity;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -15,7 +17,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')->hourly();
+         $schedule->job(new ActivityNotification)->everyTwoHours();
     }
 
     /**
