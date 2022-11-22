12bcd75e4044d1ad7671ef8300db74b44db690a9
Max Sprenkels <max@sprenkels.net>
2022-11-22 16:46:38 +0100

    formstyle

--- resources/views/activities/create.blade.php
+++ resources/views/activities/create.blade.php
@@ -1,20 +1,39 @@
-<form method="POST" action="{{route('activities.store')}}">
+<script src="https://cdn.tailwindcss.com"></script>
+<link rel="stylesheet" href="style.css">
+<div class="w-full bg-grey-500">
+        <div class="container mx-auto py-8">
+            <div class="w-96 mx-auto bg-white rounded shadow">
+
+                <div class="mx-16 py-4 px-8 text-black text-xl font-bold border-b border-grey-500 justify-center">Activiteiten
+                </div>
+
+
+<form method="POST" action="{{route('activities.store')}}" name="activiteiten" id="activiteiten">
+<div class="py-4 px-8">
+<div class="mb-4">
     @csrf
-    <label for="name_activity">
+    <label for="name_activity" class="block text-grey-darker text-sm font-bold mb-2">
         Name
-        <input type="text" name="name_activity">
+        <input type="text" name="name_activity" class=" border rounded w-full py-2 px-3 text-grey-darker">
     </label>
-
-    <label for="description_activity">
+</div>
+<div class="mb-4">
+    <label for="description_activity" class="block text-grey-darker text-sm font-bold mb-2">
         Description
-        <input type="text" name="description_activity">
+        <textarea name="description_activity" class=" border rounded w-full py-2 px-3 text-grey-darker"></textarea>
     </label>
+</div>
+<div class="mb-4">
 
-    <label for="due_date">
+    <label for="due_date" class="block text-grey-darker text-sm font-bold mb-2">
         Due Date
-        <input type="date" name="due_date_activity">
+        <input type="date" name="due_date_activity" class=" border rounded w-full py-2 px-3 text-grey-darker">
     </label>
+    <div class="mb-4">
     <input type="hidden" name="plant_id" value="1">
-    <input type="submit">
+    <input type="submit" class="mb-2 mx-16 rounded-full py-1 px-24 bg-purple-400 justify-center">
+    </div>
+</div>
+</div>
 
 </form>
