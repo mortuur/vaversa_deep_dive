52173deb9f5f98340f84e05e03c0f2738f12cf74
Max Sprenkels <max@sprenkels.net>
2022-11-23 15:37:25 +0100

    edit style

--- resources/views/activities/create.blade.php
+++ resources/views/activities/create.blade.php
@@ -1,6 +1,35 @@
 <script src="https://cdn.tailwindcss.com"></script>
 <link rel="stylesheet" href="style.css">
+
+
 <div class="flex min-h-screen items-center justify-start bg-white">
+  <div class="mx-auto w-full max-w-lg">
+    <h1 class="text-4xl font-medium text-violet-900">Vaversa</h1>
+    <p class="mt-3 text-violet-900">Activiteiten</p>
+    <!-- @csrf -->
+    <form method="POST" action="{{route('activities.store')}}" name="activiteiten" id="activiteiten" class="mt-10">
+
+      <input type="hidden" name="access_key" value="YOUR_ACCESS_KEY_HERE" /> 
+      <div class="grid gap-6 sm:grid-cols-2">
+        <div class="relative z-0">
+          <input type="text" name="name_activity" class="peer block w-full appearance-none border-0 border-b border-gray-500 bg-transparent py-2.5 px-0 text-sm text-gray-900 focus:border-blue-600 focus:outline-none focus:ring-0" placeholder=" " />
+          <label for="name_activity" class="absolute top-3 -z-10 origin-[0] -translate-y-6 scale-75 transform text-sm text-gray-500 duration-300 peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100 peer-focus:left-0 peer-focus:-translate-y-6 peer-focus:scale-75 peer-focus:text-blue-600 peer-focus:dark:text-blue-500">Name</label>
+        </div>
+        <div class="relative z-0">
+          <input type="date" name="due_date_activity" class="peer block w-full appearance-none border-0 border-b border-gray-500 bg-transparent py-2.5 px-0 text-sm text-gray-900 focus:border-blue-600 focus:outline-none focus:ring-0" placeholder=" " />
+          <label for="due_date" class="absolute top-3 -z-10 origin-[0] -translate-y-6 scale-75 transform text-sm text-gray-500 duration-300 peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100 peer-focus:left-0 peer-focus:-translate-y-6 peer-focus:scale-75 peer-focus:text-blue-600 peer-focus:dark:text-blue-500">Due Date</label>
+        </div>
+        <div class="relative z-0 col-span-2">
+          <textarea name="description_activity" rows="5" class="peer block w-full appearance-none border-0 border-b border-gray-500 bg-transparent py-2.5 px-0 text-sm text-gray-900 focus:border-blue-600 focus:outline-none focus:ring-0" placeholder=" "></textarea>
+          <label for="description_activity" class="absolute top-3 -z-10 origin-[0] -translate-y-6 scale-75 transform text-sm text-gray-500 duration-300 peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100 peer-focus:left-0 peer-focus:-translate-y-6 peer-focus:scale-75 peer-focus:text-blue-600 peer-focus:dark:text-blue-500">Description</label>
+        </div>
+      </div>
+      <input type="hidden" name="plant_id" value="1">
+      <button type="submit" class="mt-5 rounded-md bg-violet-900 px-10 py-2 text-white">Send Message</button>
+    </form>
+  </div>
+</div>
+<!-- <div class="flex min-h-screen items-center justify-start bg-white">
         <div class="mx-auto w-full max-w-lg">
         <h1 class="text-4xl font-medium">Vaversa</h1>
                 <div class="mt-3">Activiteiten
@@ -11,7 +40,7 @@
 <div class="grid gap-6 sm:grid-cols-2">
 <div class="relative z-0">
     <!-- @csrf -->
-    <label for="name_activity" class="absolute top-3 -z-10 origin-[0] -translate-y-6 scale-75 transform text-sm text-gray-500 duration-300 peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100 peer-focus:left-0 peer-focus:-translate-y-6 peer-focus:scale-75 peer-focus:text-blue-600 peer-focus:dark:text-blue-500">
+    <!-- <label for="name_activity" class="absolute top-3 -z-10 origin-[0] -translate-y-6 scale-75 transform text-sm text-gray-500 duration-300 peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100 peer-focus:left-0 peer-focus:-translate-y-6 peer-focus:scale-75 peer-focus:text-blue-600 peer-focus:dark:text-blue-500">
         Name
         <input type="text" name="name_activity" class="peer block w-full appearance-none border-0 border-b border-gray-500 bg-transparent py-2.5 px-0 text-sm text-gray-900 focus:border-blue-600 focus:outline-none focus:ring-0">
     </label>
@@ -35,4 +64,4 @@
 </div>
 </div>
 
-</form>
+</form> --> -->
--- resources/views/activities/edit.blade.php
+++ resources/views/activities/edit.blade.php
@@ -1,20 +1,33 @@
-<form method="POST" action="{{route('activities.update', $activity->id)}}">
-@csrf
-    @method('put')
-<label for="name_activity">
+<script src="https://cdn.tailwindcss.com"></script>
+<link rel="stylesheet" href="style.css">
+
+<div class="flex min-h-screen items-center justify-start bg-white">
+  <div class="mx-auto w-full max-w-lg">
+    <h1 class="text-4xl font-medium text-violet-900">Vaversa</h1>
+    <p class="mt-3 text-violet-900">Activiteiten</p>
+<form method="POST" action="{{route('activities.update', $activity->id)}}" class="mt-10">
+<!-- @csrf
+    @method('put') -->
+<input type="hidden" name="access_key" value="YOUR_ACCESS_KEY_HERE" />
+<div class="grid gap-6 sm:grid-cols-2">
+    <div class="relative z-0">
+<label for="name_activity" class="peer block w-full appearance-none border-0 border-b border-gray-500 bg-transparent py-2.5 px-0 text-sm text-gray-900 focus:border-blue-600 focus:outline-none focus:ring-0" placeholder=" ">
     Name
-    <input type="text" name="name_activity" value="{{$activity->name}}">
+    <input type="text" name="name_activity" value="{{$activity->name}}" class="peer block w-full appearance-none border-0 border-b border-gray-500 bg-transparent py-2.5 px-0 text-sm text-gray-900 focus:border-blue-600 focus:outline-none focus:ring-0" placeholder="Name">
 </label>
-
-<label for="description_activity">
-    Description
-    <input type="text" name="description_activity" value="{{$activity->description}}">
-</label>
-
-<label for="due_date">
+</div>
+<div class="relative z-0">
+<label for="due_date" class="peer block w-full appearance-none border-0 border-b border-gray-500 bg-transparent py-2.5 px-0 text-sm text-gray-900 focus:border-blue-600 focus:outline-none focus:ring-0" placeholder=" ">
     Due Date
-    <input type="date" name="due_date_activity" value="{{$activity->due_date}}">
+    <input type="date" name="due_date_activity" value="{{$activity->description}}" class="peer block w-full appearance-none border-0 border-b border-gray-500 bg-transparent py-2.5 px-0 text-sm text-gray-900 focus:border-blue-600 focus:outline-none focus:ring-0" placeholder="Name">
 </label>
+</div>
+<div class="relative z-0 col-span-2">
+<label for="description_activity" class="absolute top-3 -z-10 origin-[0] -translate-y-6 scale-75 transform text-sm text-gray-500 duration-300 peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100 peer-focus:left-0 peer-focus:-translate-y-6 peer-focus:scale-75 peer-focus:text-blue-600 peer-focus:dark:text-blue-500"></label>
+    Description
+    <textarea name="due_date_activity" rows="5" value="{{$activity->due_date}}" class="peer block w-full appearance-none border-0 border-b border-gray-500 bg-transparent py-2.5 px-0 text-sm text-gray-900 focus:border-blue-600 focus:outline-none focus:ring-0" placeholder=" "></textarea>
+</div>
+</div>
 <input type="hidden" name="plant_id" value="{{$activity->plant_id}}">
 <input type="submit">
 
