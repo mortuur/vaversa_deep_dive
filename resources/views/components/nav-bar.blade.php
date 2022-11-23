d1791c57fe1a1c6bb770522dc0071a92dc6ea0d0
Henry Benaye <57096397+HenryBenaye@users.noreply.github.com>
2022-11-23 14:38:16 +0100

    navbar url

--- resources/views/components/nav-bar.blade.php
+++ resources/views/components/nav-bar.blade.php
@@ -1,7 +1,7 @@
 
 <nav class="p-3 border-gray-200 rounded drop-shadow-xl dark:bg-white dark:border-gray-700">
     <div class="container flex flex-wrap items-center justify-between mx-auto">
-        <a href="#" class="flex items-center">
+        <a href="{{route('activities.index')}}" class="flex items-center">
             <img src="https://api.v1.iyyu.com/images/space/DyhSrd5hVZx9rph4BAhQBdoIkPx1ouEcpuKyYGb8JJ6KtTEBHlqz1YGGAvIhl6kb-480.png" class="h-6 mr-3 sm:h-10" alt="Flowbite Logo" />
         </a>
         <div class="hidden w-full md:block md:w-auto" id="navbar-solid-bg">
@@ -10,10 +10,10 @@
                     <a href="#" class="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-white dark:bg-blue-600 md:dark:bg-transparent" aria-current="page">Home</a>
                 </li>
                 <li>
-                    <a href="#" class="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-violet-800  dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Activities</a>
+                    <a href="{{route('activities.index')}}" class="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-violet-800  dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Activities</a>
                 </li>
                 <li>
-                    <a href="" class="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-violet-800 dark:hover:bg-gray-700 dark:hover:text-violet-800 md:dark:hover:bg-transparent">Plants</a>
+                    <a href="{{route('plants.index')}}" class="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-violet-800 dark:hover:bg-gray-700 dark:hover:text-violet-800 md:dark:hover:bg-transparent">Plants</a>
                 </li>
             </ul>
         </div>
